842ee75f3a2c15c7a61d94c55243f96625d6b8a0
MyComputer <ibodov.aziz@yandex.ru>
2024-12-19 15:24:00 +0300

    del property add __str__ and __eq__

---
+++ classes/video.py
@@ -0,0 +1,14 @@
+class Video:
+    def __init__(self, title: str, duration: int, adult_mode=False):
+        self.title = title
+        self.duration = duration
+        self.time_now = 0
+        self.adult_mode = adult_mode
+
+    def __str__(self):
+        return f'{self.title}'
+
+    def __eq__(self, other):
+        if isinstance(other, Video):
+            return self.title == other.title
+
